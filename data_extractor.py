ff56a7c3a648fbe45a3f25cc864148e2f536b615
Mr-Eke <c.eke@alustudent.com>
2025-02-13 21:13:40 +0200

    a class to handle data extraction

---
+++ data_extractor.py
@@ -0,0 +1,40 @@
+#!/usr/env/python3
+"""
+    This script holds a DataExtractor class to extract emails, URLs, phone numbers,
+    credit card numbers, time in 12-hour or 24-hour format, HTML tags, hashtags,
+    and currency amounts from a given API string response using regular expression.
+"""
+
+import re
+
+
+class DataExtractor:
+    """
+    class for extracting the required data from a string.
+    Attributes:
+        sample_text (str): The text/string to be processed.
+    """
+
+    def __init__(self, sample_text):
+        """ Initialize objects with the string to be processed """
+        self.sample_text = sample_text
+
+    def extract_emails(self):
+        """ Extracts and returns a list of emails """
+
+        pattern = r'\b[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Z|a-z]{2,}\b'
+        return re.findall(pattern, self.sample_text)
+
+    def extract_urls(self):
+        """ Extracts and returns a list of all URLs """
+
+        pattern = r'https?://(?:[-\w.]|(?:%[\da-fA-F]{2}))+'
+        return re.findall(pattern, self.sample_text)
+
+    def extract_phone_numbers(self):
+        """ Extractand returns a list of phone numbers """
+
+        pattern = r'\+?1?\d{9,15}'
+        return re.findall(pattern, self.sample_text)
+
+
