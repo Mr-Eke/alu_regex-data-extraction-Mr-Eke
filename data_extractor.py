4b7affb7e6d4099fcf663c813a56b53d819a2a7a
Mr-Eke <c.eke@alustudent.com>
2025-02-14 16:49:18 +0200

    Typing: handled statically

--- data_extractor.py
+++ data_extractor.py
@@ -15,29 +15,29 @@ class DataExtractor:
         sample_text (str): The text/string to be processed.
     """
 
-    def __init__(self, sample_text):
+    def __init__(self, sample_text: str) -> None:
         """ Initialize objects with the string to be processed """
         self.sample_text = sample_text
 
-    def extract_emails(self):
+    def extract_emails(self) -> list:
         """ Extracts and returns a list of emails """
 
         pattern = r'\b[A-Za-z0-9_%+-]+(?:\.[A-Za-z0-9_%+-]+)*@[A-Za-z0-9-]+(?:\.[A-Za-z0-9-]+)*\.[A-Za-z]{2,}\b'
         return re.findall(pattern, self.sample_text)
 
-    def extract_urls(self):
+    def extract_urls(self) -> list:
         """ Extracts and returns a list of all URLs """
 
         pattern = r'https?://(?:(?:[-\w]+\.)+[a-zA-Z]{2,}|localhost|\d{1,3}(?:\.\d{1,3}){3})(?::\d{1,5})?(?:/[-\w%.~+]*)*(?:\?[-\w%.~+=&]*)?(?:#[-\w%]*)?'
         return re.findall(pattern, self.sample_text)
 
-    def extract_phone_numbers(self):
+    def extract_phone_numbers(self) -> list:
         """ Extract and returns a list of phone numbers """
 
         pattern = r'\+?\d{1,3}[-.\s]?\d{3,4}[-.\s]?\d{3}[-.\s]?\d{3,4}'
         return re.findall(pattern, self.sample_text)
 
-    def extract_credit_cards(self):
+    def extract_credit_cards(self) ->list:
         """Extracts and returns a list of credit card numbers."""
 
         pattern = r'\b(?:\d{4}[-\s]?){3}\d{4}\b'
