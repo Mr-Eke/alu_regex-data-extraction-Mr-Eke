a48d2987c60dcf4f5070800278e597931ff144e2
Mr-Eke <c.eke@alustudent.com>
2025-02-15 16:55:56 +0200

    pep8 validated

--- data_extractor.py
+++ data_extractor.py
@@ -13,56 +13,46 @@ class DataExtractor:
     Attributes:
         sample_text (str): The text/string to be processed.
     """
-
     def __init__(self, sample_text: str) -> None:
         """ Initialize objects with the string to be processed """
         self.sample_text: str = sample_text
 
     def extract_emails(self) -> list[str]:
         """ Extracts and returns a list of emails """
-
         pattern = r'\b[A-Za-z0-9_%+-]+(?:\.[A-Za-z0-9_%+-]+)*@[A-Za-z0-9-]+(?:\.[A-Za-z0-9-]+)*\.[A-Za-z]{2,}\b'
         return re.findall(pattern, self.sample_text)
 
     def extract_urls(self) -> list[str]:
         """ Extracts and returns a list of all URLs """
-
         pattern = r'https?://(?:(?:[-\w]+\.)+[a-zA-Z]{2,}|localhost|\d{1,3}(?:\.\d{1,3}){3})(?::\d{1,5})?(?:/[-\w%.~+]*)*(?:\?[-\w%.~+=&]*)?(?:#[-\w%]*)?'
         return re.findall(pattern, self.sample_text)
 
     def extract_phone_numbers(self) -> list[str]:
         """ Extract and returns a list of phone numbers """
-
         pattern = r'\+?\d{1,3}[-.\s]?\d{3,4}[-.\s]?\d{3}[-.\s]?\d{3,4}'
         return re.findall(pattern, self.sample_text)
 
     def extract_credit_cards(self) -> list[str]:
         """Extracts and returns a list of credit card numbers."""
-
         pattern = r'\b(?:\d{4}[-\s]?){3}\d{4}\b'
         return re.findall(pattern, self.sample_text)
 
     def extract_time(self) -> list[str]:
         """Extracts and returns a list of time formats."""
-
         pattern = r'\b((?:[01]?\d|2[0-3]):[0-5]\d(?:\s?[APap][Mm])?|\b(?:0?[1-9]|1[0-2]):[0-5]\d\s?[APap][Mm])\b'
         return re.findall(pattern, self.sample_text)
 
     def get_html_tags(self) -> list:
         """ Returns a list of all matching HTML tags."""
-
         pattern = r'<[^>]+>'
         return re.findall(pattern, self.sample_text)
 
     def get_hashtags(self) -> list[str]:
         """ Extract and returns a list of all matching hashtags."""
-
         pattern = r'#[A-Za-z0-9_]+'
         return re.findall(pattern, self.sample_text)
 
-
     def get_currency_amounts(self) -> list:
         """ Returns a list of all matching currency amounts."""
-
         pattern = r'\$\d{1,3}(?:,\d{3})*(?:\.\d{2})?'
         return re.findall(pattern, self.sample_text)
--- test_cases.py
+++ test_cases.py
@@ -5,57 +5,57 @@ from data_extractor import DataExtractor
 
 # Sample text/string to extract relevant data from
 test_string = """
-======= Edge Cases: Invalid Email Addresses ========
-o'connor@example.com "very.unusual.@.unusual.com"@example.com
-"user\\\"name"@example.com user@[192.168.1.1]
-My email is user(at)example.com user@@example.com user@example..com.
-user@, user.example.com  # (Invalid: missing @)
-example.emailaddress.com  # (Invalid: looks like a domain, not an email)
-"email": "user@example,com"  # (Invalid: comma instead of dot)
-
-============ Valid Email Addresses =============
-eke-chi.me@example.co.uk alt email: eke101.c@alustudent.com
-Contact me at c.eke@alustudent.com and for any support inquiries,
-reach out to support@alueducation.com. emails in bracket (braces@example.org)
-
- ========= Edge Cases: Invalid URLs (Not Matched) =========
-    "http:/example.com",        - Missing slash
-    "https//example.com",       - Missing colon
-    "://example.com",           - Missing protocol
-    "https://", https://        - Missing domain
-    "https://.com",             - Missing domain name
-    "https://example",          - Missing TLD
-     https://.eke.com",         - Domain starts with dot
-    "https://example..com",     - Consecutive dots
-    "https://exam ple.com",     - Space in domain
-    "https://exam_ple.com",     - Underscore in domain name (invalid)
-
-============== Valid Urls (Matched) ===============
-"https://eke.com/#", https://eke.com. checkout my socials and connect
- with me at https://www.linkedin.com/in/chiago/, http://www.eke-chi.tech
-
-========== Edge Cases: Phone numbers (Different formats) =========
-(123) 456-7890 or call this number 123-456-7890 123.456.7890.
-you can reach Herve at +250788899944, +1 123-456-7890.
-
-==================== credit Card =====================
-1234 5678 9012 3456,1234-5678-9012-3456
-Invalid (Morethan a credit card lenght): 1234-5678-9012-345655
-
-==================== Valid Time Formats =====================
-14:30 02:30 pm, 01:30 am - 03:01AM*04:01 PM
-
-============ Invalid Time formats (Not matched) ============
- 15:60 24:30  2: 20,  5:10  PM, 01:30  am
-
- ========= HTML tags: ========
-<p><div class="example">, <img src="image.jpg" alt="description"/>
-
-============== Hashtags: ==============
-#example #ThisIsAHashtag-#AluSWE
-
-========== Currency amounts: ===========
-$19.99 $1,234.56
+    ======= Edge Cases: Invalid Email Addresses ========
+    o'connor@example.com "very.unusual.@.unusual.com"@example.com
+    "user\\\"name"@example.com user@[192.168.1.1]
+    My email is user(at)example.com user@@example.com user@example..com.
+    user@, user.example.com  # (Invalid: missing @)
+    example.emailaddress.com  # (Invalid: looks like a domain, not an email)
+    "email": "user@example,com"  # (Invalid: comma instead of dot)
+    
+    ============ Valid Email Addresses =============
+    eke-chi.me@example.co.uk alt email: eke101.c@alustudent.com
+    Contact me at c.eke@alustudent.com and for any support inquiries,
+    reach out to support@alueducation.com. emails in bracket (braces@example.org)
+    
+     ========= Edge Cases: Invalid URLs (Not Matched) =========
+        "http:/example.com",        - Missing slash
+        "https//example.com",       - Missing colon
+        "://example.com",           - Missing protocol
+        "https://", https://        - Missing domain
+        "https://.com",             - Missing domain name
+        "https://example",          - Missing TLD
+         https://.eke.com",         - Domain starts with dot
+        "https://example..com",     - Consecutive dots
+        "https://exam ple.com",     - Space in domain
+        "https://exam_ple.com",     - Underscore in domain name (invalid)
+    
+    ============== Valid Urls (Matched) ===============
+    "https://eke.com/#", https://eke.com. checkout my socials and connect
+     with me at https://www.linkedin.com/in/chiago/, http://www.eke-chi.tech
+    
+    ========== Edge Cases: Phone numbers (Different formats) =========
+    (123) 456-7890 or call this number 123-456-7890 123.456.7890.
+    you can reach Herve at +250788899944, +1 123-456-7890.
+    
+    ==================== credit Card =====================
+    1234 5678 9012 3456,1234-5678-9012-3456
+    Invalid (Morethan a credit card lenght): 1234-5678-9012-345655
+    
+    ==================== Valid Time Formats =====================
+    14:30 02:30 pm, 01:30 am - 03:01AM*04:01 PM
+    
+    ============ Invalid Time formats (Not matched) ============
+     15:60 24:30  2: 20,  5:10  PM, 01:30  am
+    
+     ========= HTML tags: ========
+    <p><div class="example">, <img src="image.jpg" alt="description"/>
+    
+    ============== Hashtags: ==============
+    #example #ThisIsAHashtag-#AluSWE
+    
+    ========== Currency amounts: ===========
+    $19.99 $1,234.56
 """
 
 # creat an instance of DataExtractor with the sample string
